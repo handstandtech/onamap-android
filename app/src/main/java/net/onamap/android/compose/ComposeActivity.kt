78fb45d79e14a0cbb2a9591e929fe651fc5142ad
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2020-08-11 21:52:40 -0400

    Images are working!

--- app/src/main/java/net/onamap/android/compose/ComposeActivity.kt
+++ app/src/main/java/net/onamap/android/compose/ComposeActivity.kt
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.preferredHeightIn
 import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.layout.preferredSizeIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.loadImageResource
@@ -22,27 +29,34 @@ class ComposeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Column {
-                        Row {
-                            Greeting("Androi5")
-                        }
-                        Row {
-                            Greeting("Android1")
-                        }
-                        Row {
-                            Greeting("Android2")
-                        }
-                        CoilImage(data = "https://loremflickr.com/300/300")
-                        imageResource(id = R.drawable.ak)
-                        
-                        val image = loadImageResource(id = R.drawable.ak)
-                        image.resource.resource?.let {
-                            Image(asset=it, modifier = Modifier.preferredSize(200.dp))
-                        }
-                    }
-                }
+                UsState()
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    color = MaterialTheme.colors.primary
+//                ) {
+//                    Column {
+//                        Row {
+//                            Greeting("Androi5")
+//                        }
+//                        Row {
+//                            Greeting("Android1")
+//                        }
+//                        Row {
+//                            Greeting("Android2")
+//                            CoilImage(
+//                                data = "https://loremflickr.com/200/200",
+////                            contentScale = ContentScale.FillWidth,
+//                                modifier = Modifier.fillMaxSize()
+//                            )
+//                        }
+//                        Image(imageResource(id = R.drawable.ak))
+//
+//                        val image = loadImageResource(id = R.drawable.ak)
+//                        image.resource.resource?.let {
+//                            Image(asset = it, modifier = Modifier.preferredSize(200.dp))
+//                        }
+//                    }
+//                }
             }
         }
     }
--- app/src/main/java/net/onamap/android/compose/ComposeFrameLayout.kt
+++ app/src/main/java/net/onamap/android/compose/ComposeFrameLayout.kt
@@ -2,20 +2,34 @@ package net.onamap.android.compose
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.widget.FrameLayout
+import androidx.compose.foundation.Border
+import androidx.compose.foundation.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.VerticalScroller
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Recomposer
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.ui.tooling.preview.Preview
 import dev.chrisbanes.accompanist.coil.CoilImage
 import dev.chrisbanes.accompanist.coil.CoilImageWithCrossfade
@@ -42,31 +56,40 @@ class ComposeFrameLayout @JvmOverloads constructor(
 @Composable
 fun UsState() {
     // A surface container using the 'background' color from the theme
-    Surface(color = MaterialTheme.colors.background) {
-        Column {
-            Row {
-                Greeting("Row 1")
-                MyButton()
-            }
-            Row {
-                Greeting("Row 2")
-                MyButton()
-            }
-            Row {
-
-                Greeting("Image")
-                CoilImage(
-                    data = "https://compose.academy/doks-theme/assets/images/layout/logo.png"
+    Surface(
+        color = MaterialTheme.colors.background,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        ScrollableColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalGravity = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Alaska",
+                style = TextStyle(fontSize = 40.sp),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Box(
+                backgroundColor = Color.Green,
+                gravity = Alignment.Center,
+                modifier = Modifier.wrapContentWidth(),
+                shape = RoundedCornerShape(10),
+                border = Border(
+                    size = 1.dp,
+                    color = Color.DarkGray
                 )
-                CoilImageWithCrossfade(
-                    data = "https://loremflickr.com/300/300",
-                    onRequestCompleted = {
-                        Log.d(TAG, "Request Completed")
-                    }
+            ) {
+                Image(
+                    asset = imageResource(id = R.drawable.ak),
+                    modifier = Modifier.size(200.dp, 200.dp)
                 )
             }
+            CoilImage(
+                data = "https://live.staticflickr.com/3826/11697289533_cfd6282b37.jpg",
+                modifier = Modifier.size(500.dp, 300.dp)
+            )
         }
-
     }
 }
 
@@ -74,7 +97,10 @@ private val TAG = "TAG"
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        style = TextStyle(fontSize = 20.sp)
+    )
 }
 
 @Composable
