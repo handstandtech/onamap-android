4b0e8a97ecb0f94cd46e084df8c116174c35fdc0
Sam Edwards <264948+handstandsam@users.noreply.github.com>
2025-04-27 21:29:41 -0400

    Version catalogs

--- webapp/build.gradle.kts
+++ webapp/build.gradle.kts
@@ -1,16 +1,9 @@
-val kotlin_version="2.1.20"
-val logback_version="1.4.14"
-val ktor_version="3.1.2"
-
 plugins {
     kotlin("jvm")
     id("io.ktor.plugin") version "3.1.2"
     // kotlin("serialization")
     libs.plugins.kotlin.serialization
 }
-
-group = "com.handstandsam.app"
-version = "0.0.1"
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
@@ -20,11 +13,11 @@ application {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-config-yaml")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-thymeleaf-jvm")
+    implementation(libs.ktor.server.core.jvm)
+    implementation(libs.ktor.server.netty.jvm)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    implementation(libs.ktor.server.content.negotiation.jvm)
+    implementation(libs.ktor.serialization.kotlinx.json.jvm)
+    implementation(libs.ktor.server.thymeleaf.jvm)
 }
